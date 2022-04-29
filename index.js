5f157f8e5dcf9fefa9c10e98496b5d17e14e49c7
Muhammad SA <MuhammadSA211999>
2022-04-29 20:08:10 +0600

    sigle api done

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 5000
+
+const verifyToken = (req, res, next) => {
+    const tokenWithBearer = req.headers.authorization
+    if (!tokenWithBearer) {
+        res.status(401).send({ success: false, error: 'unauthorized access' })
+    }
+    const secretToken = tokenWithBearer.split(' ')[1]
+    jwt.verify(secretToken, process.env.SECRET_TOKEN, (error, decoded) => {
+        if (error) {
+            res.status(403).send({ success: 0, message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 const uri = "mongodb+srv://muhammad-sa:eqDocytc6pHtyoIS@cluster0.jjaj9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -13,6 +30,14 @@ async function run() {
     try {
         await client.connect()
         const servicesCollection = client.db("servicess").collection("service")
+        // make a token 
+        app.post('/login', async (req, res) => {
+            const email = req.body
+            const secretToken = jwt.sign(email, process.env.SECRET_TOKEN, { expiresIn: '2d' })
+            res.send({ secretToken })
+            // res.status(200).send({data:secretToken,message:'successfully token make'})
+        })
+
 
         // info post from client
         app.post('/addGadget', async (req, res) => {
@@ -34,8 +59,8 @@ async function run() {
             res.send(gadgets)
         })
 
-        // singleActivity by id
-        app.get('/activity/:id', async (req, res) => {
+        // singleGadget by id
+        app.get('/gadget/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const activity = await servicesCollection.findOne(query)
@@ -43,7 +68,7 @@ async function run() {
         })
 
         //user activity delete
-        app.delete('/delete-a-activity/:id', async (req, res) => {
+        app.delete('/deleteGadget/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await servicesCollection.deleteOne(query)
@@ -51,13 +76,32 @@ async function run() {
             console.log(result.insertedId);
         })
 
-        // user all activity 
-        app.get('/userActivity', async (req, res) => {
+        // user all items
+        app.get('/myGadgets', async (req, res) => {
             const email = req.query.email
+            // const tokenEmail = req.decoded.email
+            // if (tokenEmail === email) {
             const query = { email: email }
             const cursor = servicesCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
+            // }
+        })
+        // update a gadget
+        app.put('/updateGadget/id', async (req, res) => {
+            const gadgetInfo = req.body
+            const gadgetId = req.params.id
+            if (!gadgetId && (!gadgetInfo.name || !gadgetInfo.email || !gadgetInfo.price || !gadgetInfo.supplier)) {
+                return res.send({ success: false, result: 0, error: "Please gives necessary info correctly!" })
+            }
+            const filter = { _id: (ObjectId(id)) }
+            const option = { upsert: true }
+            const updateGadget = {
+                $set: { ...gadgetInfo }
+            }
+            const result = await servicesCollection.updateOne(filter, option, updateGadget)
+            res.send({ success: true, result: result, message: 'Well done!' })
+
         })
 
     }
